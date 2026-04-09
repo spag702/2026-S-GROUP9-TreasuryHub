e7403916e4e585ee155ddb0ce222b02b189ea72e
TCoup <tracyhear510@gmail.com>
2026-04-09 10:41:05 -0700

    Fixing filename issue

--- src/components/OrgDropdown.tsx
+++ src/components/OrgDropdown.tsx
@@ -12,7 +12,7 @@ type Props = {
     basePath: string // e.g. '/dashboard' or '/files'
 }
 
-export default function OrgSwitcher({ organizations, currentOrgId, basePath }: Props) {
+export default function OrgDropDown({ organizations, currentOrgId, basePath }: Props) {
     const router = useRouter();
     return (
         <div>
@@ -20,11 +20,11 @@ export default function OrgSwitcher({ organizations, currentOrgId, basePath }: P
             <select
                 value={currentOrgId}
                 onChange={(e) => router.push(`${basePath}?orgId=${e.target.value}`)}
-                className="rounded border px-3 py-1 text-sm transition focus:ring focus:ring-blue-300"
+                className="rounded border px-3 py-1 text-sm transition focus:ring focus:ring-blue-300 bg-blue-600 text-white hover:bg-blue-700 active:scale-95"
                 id="org-select"
             >
                 {organizations.map((org) => (
-                    <option key={org.org_id} value={org.org_id}>
+                    <option key={org.org_id} value={org.org_id} className="bg-gray-200 text-black">
                         {org.org_name}
                     </option>
                 ))}
